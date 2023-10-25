5f652873a9eba5e22ec51c5350e152141e5f57fc
micah-roberson <miwirobe@iu.edu>
2023-10-25 00:45:40 -0400

    yuh

--- db.js
+++ db.js
@@ -3,16 +3,12 @@ const { Pool } = require("pg");
 const pool = new Pool({
     //user: "postgres",
     user: "postgres",
-<<<<<<< Updated upstream
-    host: "172.31.13.187",
-=======
     host: "localhost",
->>>>>>> Stashed changes
     //password: "password",
     password: "password",
     //host: "localhost",
     port: 5432,
-    database: "postgres"
+    database: "einstein"
 });
 
 module.exports = pool;
