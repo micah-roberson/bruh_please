b8ee7229795a6561a9f9667dcbc24fae5dc461d2
micah-roberson <miwirobe@iu.edu>
2023-12-20 18:49:50 -0500

    fghjk

--- merger2.py
+++ merger2.py
@@ -4,12 +4,16 @@ import ast
 # Read the CSV file
 df = pd.read_csv('your_output_folder.csv')
 
+# Define a function to remove commas and single quotes from a list of strings
+def clean_list(lst):
+    return [item.replace(',', '').replace("'", "") for item in lst]
+
 # Group by 'title' and aggregate the values
 aggregated_df = df.groupby('title').agg({
     'type': 'first',
     'info': lambda x: [ast.literal_eval(item) for item in x],
-    'meal_plans': lambda x: sum([ast.literal_eval(item) for item in x], []),
-    'dinner_1_list': lambda x: sum([ast.literal_eval(item) for item in x], [])  # Add aggregation for dinner_1_list
+    'meal_plans': lambda x: clean_list(sum([ast.literal_eval(item) for item in x], [])),
+    'dinner_1_list': lambda x: clean_list(sum([ast.literal_eval(item) for item in x], []))
 }).reset_index()
 
 # Export the result to a new CSV file
